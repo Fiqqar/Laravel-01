ef4feae4e1a76eac87770ae748d8e7e52566c054
fiq <145265758+Fiqqar@users.noreply.github.com>
2025-08-28 10:35:11 +0700

    28-08-25 (add components)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
-
-    <div class="container py-5">
-        <div class="text-center">
-            <h1 class="mb-3">Selamat Datang di Website Saya</h1>
-            <p class="lead">Laravel First Project🚀</p>
-        </div>
-    </div>
-
-</body>
-</html>
+<x-layout title="Home">
+    <h1 class="text-2xl font-semibold text-gray-900">Welcome to the Home Page</h1>
+    <p class="mt-4 text-gray-600">Homepage using Laravel component</p>
+</x-layout>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
+<x-layout title="Profile">
+    <h1 class="mb-3 font-bold">Profile Page</h1>
 
-    <div class="container py-5">
-            <h1 class="mb-3 text-center">Profile Page</h1>
-
-            <hr>
+            <hr class="mb-5">
 
             <ul class="list-group">
                 <li class="list-group-item"><strong>Name :</strong> {{ $name }}</li>
                 <li class="list-group-item"><strong>Class :</strong> {{ $class }}</li>
                 <li class="list-group-item"><strong>Major :</strong> {{ $major }}</li>
             </ul>
-    </div>
-
-</body>
-</html>
+</x-layout>
